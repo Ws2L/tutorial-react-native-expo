cb61217b24906c53de746ac6e933b789510827d4
Weslley <souza.weslley@aluno.ifsp.edu.br>
2025-08-05 15:26:06 -0300

    inicio de projeto react native

--- App.js
+++ App.js
@@ -4,7 +4,12 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.box1}> </View>
+      <Text style={styles.tituloPrincipal}>Titulo Principal</Text>
+      <Text style={styles.subTitulo}>Subtitulo</Text>
+      <Text>Paragráfo</Text>
+      <Text style={styles.saibaMais}>Saiba Mais</Text>
+      <View style={styles.box2}> </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +18,43 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'white',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
+  },
+
+  tituloPrincipal: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginVertical: 20
+  },
+
+  subTitulo: {
+    fontSize: '18px',
+    fontWeight: 'bold',
+    marginVertical: 20
+  },
+
+  saibaMais: {
+    fontSize: 14,
+    fontWeight: '#464646'
+  },
+
+  box1: {
+    width: '100%',
+    height: 100,
+    backgroundColor: 'red'
+  },
+
+  box2: {
+    width: '100%',
+    height: 100,
+    backgroundColor: 'black'
+  },
+
+  box3: {
+    width: 50,
+    height: 50,
+    backgroundColor: 'red'
   },
 });
