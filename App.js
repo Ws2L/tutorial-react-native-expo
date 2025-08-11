74adce469e9280d7966fe8153c7a23f267b567b6
Weslley <souza.weslley@aluno.ifsp.edu.br>
2025-08-11 17:56:16 -0300

    configurando layout

--- App.js
+++ App.js
@@ -1,60 +1,117 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text, StyleSheet } from "react-native"
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <View style={styles.box1}> </View>
-      <Text style={styles.tituloPrincipal}>Titulo Principal</Text>
-      <Text style={styles.subTitulo}>Subtitulo</Text>
-      <Text>Paragráfo</Text>
-      <Text style={styles.saibaMais}>Saiba Mais</Text>
-      <View style={styles.box2}> </View>
-      <StatusBar style="auto" />
+      <View style={styles.header}>
+        <Text>Logo</Text>
+        <View style={styles.menu}></View>
+      </View>
+
+      <View style={styles.content}>
+
+        <View style={styles.card}>
+          <View style={styles.image}></View>
+          <View style={styles.text}>
+            <Text style={styles.title}>BOLO DE CENOURA COM COBERTURA</Text>
+            <Text style={styles.p}>A sobremesa brasileiríssima que é uma verdadeira alegria para o paladar, onde cada pedaço é um suspiro.
+            Uma maravilha culinária que você não pode deixar de experimentar!
+            Já pensou no que uma sobremesa dessas pode causar na sua mesa?
+            Só alegria!
+            </Text>
+          </View>
+        </View>
+
+        <View style={styles.card}>
+          <View style={styles.image}></View>
+          <View style={styles.text}>
+            <Text style={styles.title}>BOLO DE CENOURA COM COBERTURA</Text>
+            <Text style={styles.p}>A sobremesa brasileiríssima que é uma verdadeira alegria para o paladar, onde cada pedaço é um suspiro.
+            Uma maravilha culinária que você não pode deixar de experimentar!
+            Já pensou no que uma sobremesa dessas pode causar na sua mesa?
+            Só alegria!
+            </Text>
+          </View>
+        </View>
+
+        <View style={styles.card}>
+          <View style={styles.image}></View>
+          <View style={styles.text}>
+            <Text style={styles.title}>BOLO DE CENOURA COM COBERTURA</Text>
+            <Text style={styles.p}>A sobremesa brasileiríssima que é uma verdadeira alegria para o paladar, onde cada pedaço é um suspiro.
+            Uma maravilha culinária que você não pode deixar de experimentar!
+            Já pensou no que uma sobremesa dessas pode causar na sua mesa?
+            Só alegria!
+            </Text>
+          </View>
+        </View>
+
+      </View>
     </View>
-  );
+  )
 }
 
-const styles = StyleSheet.create({
+const styles = StyleSheet.create ({
   container: {
+    width: "100%",
     flex: 1,
-    backgroundColor: 'white',
-    alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  
+  header: {
+    flex: 1,
+    width: "100%",
+    backgroundColor: "#79ff79ff",
+    justifyContent: "space-between",
+    alignItems: "center",
+    flexDirection: "row",
+    paddingHorizontal: 20
+  },
+  
+  content: {
+    flex: 8,
+    width: "100%",
+    backgroundColor: "#fff7c6ff",
+    padding: 10,
+    
   },
 
-  tituloPrincipal: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    marginVertical: 20
+  menu: {
+    width: 40,
+    height: 40,
+    backgroundColor: "#ffffffff"
   },
 
-  subTitulo: {
-    fontSize: '18px',
-    fontWeight: 'bold',
-    marginVertical: 20
+  card: {
+    width: "100%",
+    height: 200,
+    backgroundColor: "#ffd6d6ff",
+    padding: 10,
+    flexDirection: "row",
+    marginBottom: 30
   },
 
-  saibaMais: {
-    fontSize: 14,
-    fontWeight: '#464646'
+  image: {
+    width: 80,
+    height: 80,
+    backgroundColor: "white",
   },
 
-  box1: {
-    width: '100%',
-    height: 100,
-    backgroundColor: 'red'
+  text: {
+    flex: 1,
+    paddingHorizontal: 10,
+    flexDirection: "column"
+    //width: "100%"
   },
 
-  box2: {
-    width: '100%',
-    height: 100,
-    backgroundColor: 'black'
+  title: {
+    fontWeight: "Bold",
+    color: "white"
   },
 
-  box3: {
-    width: 50,
-    height: 50,
-    backgroundColor: 'red'
+  p: {
+    color: "white",
+    textAlign: "justify"
   },
-});
+})
---
+++ README.md
@@ -0,0 +1,13 @@
+# Comandos
+- npm init // Cria o package.json para iniciar o projeto node
+
+# Comandos úteis
+- npx create-expo-app@latest --template // cria o projeto React Native com Expo
+
+# Comandos Expo
+- npm start (roda o projeto no expo)
+- npx expo start --tunnel (so em situações onde o dispositivo não está conectado na mesma rede do computador)
+
+# Comandos Git
+- git config --global user.name
+- git config --global user.email
